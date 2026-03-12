eeca3bc3c27653c85e2dc9d16a569ca0d582deab
DogBrother <roys99141@gmail.com>
2026-03-12 11:49:08 +0800

    add question and choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) #CASCADE問題珊除，選項一起刪除
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
