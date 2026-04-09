5a03d7337efd702b2dc4bed6c5dd10199c353547
DogBrother <roys99141@gmail.com>
2026-04-09 10:20:43 +0800

    哈哈

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
@@ -12,13 +12,20 @@ class IndexView(generic.ListView):
 
 
     def get_queryset(self):      
-        return Question.objects.order_by("-pub_date")[:5]
+        # return Question.objects.order_by("-pub_date")[:5]
+        #修改為今天發布以前的問題
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
-
+    #改寫方法，資料提前過濾
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 
